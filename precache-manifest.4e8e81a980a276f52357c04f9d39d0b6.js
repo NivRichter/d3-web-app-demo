18b269a93397ef4814f19194ee6d1c42bec6da5d
NivRichter <nivrichter@gmail.com>
2020-10-09 13:34:21 +0300

    Updates

---
+++ precache-manifest.4e8e81a980a276f52357c04f9d39d0b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382af560050d56491daf2a9e2a9f13e3",
+    "url": "/d3-web-app-demo/index.html"
+  },
+  {
+    "revision": "51659d3eab61163a6e34",
+    "url": "/d3-web-app-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58cd299c1005d33fe66f",
+    "url": "/d3-web-app-demo/static/js/2.e752c38f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/d3-web-app-demo/static/js/2.e752c38f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51659d3eab61163a6e34",
+    "url": "/d3-web-app-demo/static/js/main.4d421276.chunk.js"
+  },
+  {
+    "revision": "96aab5724bda2daac398",
+    "url": "/d3-web-app-demo/static/js/runtime-main.36d605f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/d3-web-app-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
